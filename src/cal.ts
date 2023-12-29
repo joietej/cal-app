f1c95b11bc671dc278c533bcf269aec388540d2e
Tejas <tejas.sahasrabudhe@gmail.com>
2023-12-29 17:38:27 +0530

    update calendar

--- src/cal.ts
+++ src/cal.ts
@@ -1,13 +1,13 @@
 import ical, { ICalCalendarMethod } from "ical-generator";
 
-const calendar = ical({ name:"foo", description:"my first iCal" });
+const calendar = ical({ name:"Sample Calendar", description:"My First iCal" });
 
-calendar.source("http://localhost:3000/cal.ics")
-calendar.url("http://localhost:3000/cal.ics")
-calendar.prodId({company:"foo", product:"Google Calendar 70.9054"})
+calendar.source("https://cal-app.fly.dev/cal")
+calendar.url("https://cal-app.fly.dev/cal")
+calendar.prodId({company:"Sample", product:"SampleCal"})
 calendar.method(ICalCalendarMethod.PUBLISH)
 calendar.scale('gregorian')
-calendar.timezone("America/New_York")
+calendar.timezone("Asia/Kolkata")
 calendar.ttl(60)
 
 const startTime = new Date();
@@ -18,7 +18,7 @@ calendar.createEvent({
   end: endTime,
   summary: "Example Event",
   description: "It works",
-  location: "my room",
+  location: "My room",
 });
 
 export default calendar;
